8c10ee25ed19bfae8313a74380d760ffa6e163f6
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-24 18:17:41 +0300

    modification

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
 
-// Write your code here
+//random
+let random;
+function generateRandom() {
+  random = Math.floor(Math.random() * 100) + 1;
+}
+generateRandom();
+
+//mod
+let num3 = 15;
+let num4 = 11;
+let mod = num3 % num4;
+
+//max
+let numbers = [10, 15, 20, 5];
+let max = Math.max(...numbers);
